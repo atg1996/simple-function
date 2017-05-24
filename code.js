e628a248ba380ab47cc5b0e46e94d506b678624b
atg1996 <t.asatryan96@gmail.com>
2017-05-24 11:49:21 +0400

    simple function

---
+++ code.js
@@ -0,0 +1,12 @@
+ console.log ('------------------------------------');
+ console.log('I am a simple JS code')
+ console.log('Give me numbers and I will give you the discriminant');
+
+ console.log('--------------------------------------- \n\n\n' );
+ var calcD = function calcD (a, b, c) {
+	return b * b - 4 * a * c;
+	}
+
+ var calcD  = calcD (1, -3, -1);
+ console.log(calcD);
+ console.log('*********************************************')
